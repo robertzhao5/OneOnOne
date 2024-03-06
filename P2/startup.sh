dba7ac0a7edb8a669245f04e840e39e55a6bb4a9
oceanchen <oceanchenn@gmail.com>
2024-03-05 23:28:34 -0500

    startup script

---
+++ P2/startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# create venv in venv folder
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install Django==4.2
+
+if [ -f manage.py ]; then
+    # If manage.py exists, run makemigrations and migrate
+    python manage.py makemigrations
+    python manage.py migrate
+else
+    echo "manage.py not found, skipping migrations."
+fi
+
+pip install Pillow
+
+deactivate
