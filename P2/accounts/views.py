5695f23f8a5149a54a1457859e917a5dad0536b0
Ocean-Ch <oceanchenn@gmail.com>
2024-03-10 19:03:31 -0400

    user registration and serialization

--- P2/accounts/views.py
+++ P2/accounts/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserRegistrationSerializer
 
-class RegisterView:
+
+# view for handing User Registrations
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "message": "Account created successfully"
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
