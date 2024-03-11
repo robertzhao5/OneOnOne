df0241d7a1b2744b48cfb2170acef7709ab8d0d5
Ocean-Ch <oceanchenn@gmail.com>
2024-03-10 21:45:42 -0400

    added user contact views

--- P2/contacts/views.py
+++ P2/contacts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserContactSerializer
 
-# Create your views here.
+from P2.contacts.models import UserContact
+
+
+class ListContactsView(ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserContactSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return UserContact.objects.filter(user=user)
