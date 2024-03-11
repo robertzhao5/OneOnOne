05f471f81d62b33393d41349d0ff95da63c9f5c1
Ocean-Ch <oceanchenn@gmail.com>
2024-03-10 21:29:50 -0400

    added user contact model

--- P2/contacts/models.py
+++ P2/contacts/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+# a row in this table indicates:
+# User has <contact> added as a contact
+class UserContact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='contacts')
+    contact = models.ForeignKey(User, related_name='contact_of',
+                                on_delete=models.CASCADE)
+    added_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # user cannot have themselves as contact
+        unique_together = ('user', 'contact')
+
+    def __str__(self):
+        return f"{self.user.username} has contact {self.contact.username}"
