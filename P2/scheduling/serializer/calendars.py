d106dba84da01f273d33077d6336fe7513441ba2
Robert <robert.zhao@mail.utoronto.ca>
2024-03-13 15:36:51 -0400

    changed name from events to meetings

--- P2/scheduling/serializer/calendars.py
+++ P2/scheduling/serializer/calendars.py
@@ -4,11 +4,11 @@ from rest_framework import serializers
 class CalendarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Calendar
-        fields = ['id', 'name', 'owner', 'participants', 'events']
+        fields = ['id', 'name', 'owner', 'participants', 'meetings']
         extra_kwargs = {
             'owner': {'read_only': True},
             'participants': {'required': False},
-            'events': {'required': False}
+            'meetings': {'required': False}
         }
     def create(self, validated_data):
         calendar = Calendar.objects.create(**validated_data)
@@ -18,9 +18,9 @@ class CalendarSerializer(serializers.ModelSerializer):
 class CalendarEditSerializer(serializers.ModelSerializer):
     name = serializers.CharField(max_length=100, required=False)
     participants = serializers.ListField(child=serializers.CharField(), required=False)
-    events = serializers.ListField(child=serializers.CharField(), required=False)
+    meetings = serializers.ListField(child=serializers.CharField(), required=False)
 
     def validate(self, data):
-        if not data['name'] and not data['participants'] and not data['events']:
+        if not data['name'] and not data['participants'] and not data['meetings']:
             raise serializers.ValidationError("No data to update")
         return data #TODO: verify if this is correct
