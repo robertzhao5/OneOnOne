1e9e383027033c3c7ea045fbfd579269c95c3585
oceanchen <oceanchenn@gmail.com>
2024-03-20 16:44:14 -0400

    commented out areas

--- P2/scheduling/views/suggested_schedules.py
+++ P2/scheduling/views/suggested_schedules.py
@@ -41,77 +41,77 @@ class GenerateSuggestedSchedulesView(APIView):
         return Response(serializer.data)
 
 
-def find_maximal_matchings(calendar_id, desired_meeting_duration):
-    """
-    Finds up to 5 sets of maximal matchings for meetings based on weekly
-    recurring availabilities. :param calendar_id: ID of the calendar for
-    which to generate matchings. :param desired_meeting_duration: Desired
-    duration of each meeting, in minutes. :return: A list of up to 5 matching
-    sets.
-    """
-    # get organizer and participants' weekly availabilities
-    calendar = Calendar.objects.get(pk=calendar_id)
-    user_avail, participant_avail = get_user_participant_availabilities(calendar)
+# def find_maximal_matchings(calendar_id, desired_meeting_duration):
+#     """
+#     Finds up to 5 sets of maximal matchings for meetings based on weekly
+#     recurring availabilities. :param calendar_id: ID of the calendar for
+#     which to generate matchings. :param desired_meeting_duration: Desired
+#     duration of each meeting, in minutes. :return: A list of up to 5 matching
+#     sets.
+#     """
+#     # get organizer and participants' weekly availabilities
+#     calendar = Calendar.objects.get(pk=calendar_id)
+#     user_avail, participant_avail = get_user_participant_availabilities(calendar)
+#
+#     matchings = []
+#     meeting_blocks_needed = desired_meeting_duration // 15
+#
+#     for attempt in range(5):  # Limit to 5 unique sets of matchings
+#         matching_set, unmatched_participants = [], set(
+#             participants_availabilities.keys())
+#
+#         for participant_id, weekly_times in participants_availabilities.items():
+#             for day, times in weekly_times.items():
+#                 for start_time, end_time in times:
+#                     # Assuming start_time and end_time are datetime.time objects
+#                     if check_intersection(organizer_availabilities[day],
+#                                           start_time, end_time,
+#                                           meeting_blocks_needed):
+#                         matching_set.append(
+#                             (participant_id, day, start_time, end_time))
+#                         unmatched_participants.remove(participant_id)
+#                         break  # Move to the next participant once matched
+#
+#         if not matching_set:
+#             break  # No more matchings could be found
+#         matchings.append(matching_set)
+#
+#         # For simplicity, this example doesn't alter availabilities between
+#         # attempts In a full implementation, we should randomize availabilities
+#         # to find alternative matchings
+#
+#     return matchings
 
-    matchings = []
-    meeting_blocks_needed = desired_meeting_duration // 15
 
-    for attempt in range(5):  # Limit to 5 unique sets of matchings
-        matching_set, unmatched_participants = [], set(
-            participants_availabilities.keys())
-
-        for participant_id, weekly_times in participants_availabilities.items():
-            for day, times in weekly_times.items():
-                for start_time, end_time in times:
-                    # Assuming start_time and end_time are datetime.time objects
-                    if check_intersection(organizer_availabilities[day],
-                                          start_time, end_time,
-                                          meeting_blocks_needed):
-                        matching_set.append(
-                            (participant_id, day, start_time, end_time))
-                        unmatched_participants.remove(participant_id)
-                        break  # Move to the next participant once matched
-
-        if not matching_set:
-            break  # No more matchings could be found
-        matchings.append(matching_set)
-
-        # For simplicity, this example doesn't alter availabilities between
-        # attempts In a full implementation, we should randomize availabilities
-        # to find alternative matchings
-
-    return matchings
-
-
-def check_intersection(organizer_availabilities, proposed_start, proposed_end,
-                       blocks_needed):
-    """
-    Checks if there's an intersection between organizer's availabilities and a time slot.
-    """
-
-    # check if the organizer's availability
-    # overlaps with the proposed time slot for the required duration (blocks_needed)
-    proposed_start_dt = datetime.combine(datetime.today(), proposed_start)
-    proposed_end_dt = datetime.combine(datetime.today(), proposed_end)
-
-    # Calculate the total duration required in minutes
-    total_required_duration = blocks_needed * 15
-
-    for day, availabilities in organizer_availabilities.items():
-        for start_time, end_time in availabilities:
-            start_time_dt = datetime.combine(datetime.today(), start_time)
-            end_time_dt = datetime.combine(datetime.today(), end_time)
-
-            # Check if there's an overlap
-            latest_start = max(proposed_start_dt, start_time_dt)
-            earliest_end = min(proposed_end_dt, end_time_dt)
-            delta = (earliest_end - latest_start).seconds / 60  # Duration of
-            # overlap in minutes
-
-            if delta >= total_required_duration:
-                return True
-
-    return False
+# def check_intersection(organizer_availabilities, proposed_start, proposed_end,
+#                        blocks_needed):
+#     """
+#     Checks if there's an intersection between organizer's availabilities and a time slot.
+#     """
+#
+#     # check if the organizer's availability
+#     # overlaps with the proposed time slot for the required duration (blocks_needed)
+#     proposed_start_dt = datetime.combine(datetime.today(), proposed_start)
+#     proposed_end_dt = datetime.combine(datetime.today(), proposed_end)
+#
+#     # Calculate the total duration required in minutes
+#     total_required_duration = blocks_needed * 15
+#
+#     for day, availabilities in organizer_availabilities.items():
+#         for start_time, end_time in availabilities:
+#             start_time_dt = datetime.combine(datetime.today(), start_time)
+#             end_time_dt = datetime.combine(datetime.today(), end_time)
+#
+#             # Check if there's an overlap
+#             latest_start = max(proposed_start_dt, start_time_dt)
+#             earliest_end = min(proposed_end_dt, end_time_dt)
+#             delta = (earliest_end - latest_start).seconds / 60  # Duration of
+#             # overlap in minutes
+#
+#             if delta >= total_required_duration:
+#                 return True
+#
+#     return False
 
 
 # def get_availabilities(calendar, user):
