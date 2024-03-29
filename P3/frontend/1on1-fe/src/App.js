5a5b00cb94316df20397d86b92d0d3f415c861c2
jenniferxi <jennifer.xi@mail.utoronto.ca>
2024-03-28 23:57:44 -0400

    pushing frontend folder

---
+++ P3/frontend/1on1-fe/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          CSC309 1on1
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
