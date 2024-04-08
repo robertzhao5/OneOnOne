6e2d481993ddd22417fe63b40439b1a54e4d25bf
oceanchen <oceanchenn@gmail.com>
2024-04-07 21:54:04 -0400

    new startup script and mv venv to gitignore

---
+++ P3/startup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# create venv in venv folder
+python3 -m venv venv
+source venv/bin/activate
+
+echo "venv is active..."
+
+echo "installing Django"
+pip install Django==4.2
+
+echo "installing required packages..."
+pip install Pillow
+pip install djangorestframework
+pip install djangorestframework-simplejwt
+
+
+echo "=============================================="
+echo "running db migrations..."
+$manage_path = "backend/OneonOne/manage.py"
+
+
+if [ -f "$manage_path" ]; then
+    # If manage.py exists, run makemigrations and migrate
+    python $manage_path makemigrations
+    python $manage_path migrate
+else
+    echo "manage.py not found at $manage_path, skipping migrations..."
+fi
+echo "=============================================="
