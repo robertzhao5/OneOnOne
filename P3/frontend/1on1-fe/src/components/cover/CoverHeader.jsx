65c40c161657a50e77b7ded195c4e3c447c33c5e
oceanchen <oceanchenn@gmail.com>
2024-04-08 19:44:36 -0400

    refactored header to use nav-link

--- P3/frontend/1on1-fe/src/components/cover/CoverHeader.jsx
+++ P3/frontend/1on1-fe/src/components/cover/CoverHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../../styles/main-header.css';
 
 function CoverHeader() {
@@ -8,10 +8,18 @@ function CoverHeader() {
             <div>
                 <h3 className="float-md-start mb-0">1-On-1</h3>
                 <nav className="nav nav-masthead justify-content-center float-md-end">
-                    <Link className="nav-link fw-bold py-1 px-0 active" to="/">Home</Link>
-                    <Link className="nav-link fw-bold py-1 px-0" to="/login">Log in</Link>
-                    <Link className="nav-link fw-bold py-1 px-0" to="/signup">Sign up</Link>
-                    <Link className="nav-link fw-bold py-1 px-0" to="/about">About</Link>
+                    <NavLink
+                        className={({ isActive }) => "nav-link fw-bold py-1 px-0" + (isActive ? " active" : "")}
+                        to="/">Home</NavLink>
+                    <NavLink
+                        className={({ isActive }) => "nav-link fw-bold py-1 px-0" + (isActive ? " active" : "")}
+                        to="/login">Log in</NavLink>
+                    <NavLink
+                        className={({ isActive }) => "nav-link fw-bold py-1 px-0" + (isActive ? " active" : "")}
+                        to="/signup">Sign up</NavLink>
+                    <NavLink
+                        className={({ isActive }) => "nav-link fw-bold py-1 px-0" + (isActive ? " active" : "")}
+                        to="/about">About</NavLink>
                 </nav>
             </div>
         </header>
