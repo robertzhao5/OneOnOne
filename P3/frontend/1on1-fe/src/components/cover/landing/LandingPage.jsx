99d580aabe0cb97330971ea0036dcfeab3b621cc
oceanchen <oceanchenn@gmail.com>
2024-04-08 17:39:26 -0400

    dynamically add bootstrap on mount

--- P3/frontend/1on1-fe/src/components/cover/landing/LandingPage.jsx
+++ P3/frontend/1on1-fe/src/components/cover/landing/LandingPage.jsx
@@ -1,16 +1,25 @@
 // LandingPage.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import CoverHeader from '../CoverHeader';
 import MainContent from './MainContent';
 import Footer from '../CoverFooter';
 import '../../../styles/cover.css';
 
 function LandingPage() {
+    useEffect(() => {
+        // add bootstrap classes to body on mount
+        document.body.classList.add("d-flex", "h-100", "text-center", "text-bg-dark");
+        // remove bt classes on unmount
+        return () => {
+            document.body.classList.remove("d-flex", "h-100", "text-center", "text-bg-dark");
+        }
+    }, []); // empty dep so effect runs only once on mount
+
     return (
         <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-            <CoverHeader />
-            <MainContent />
-            <Footer />
+            <CoverHeader/>
+            <MainContent/>
+            <Footer/>
         </div>
     );
 }
--- P3/frontend/1on1-fe/src/components/cover/landing/MainContent.jsx
+++ P3/frontend/1on1-fe/src/components/cover/landing/MainContent.jsx
@@ -1,7 +1,7 @@
 // MainContent.js
 import React from 'react';
 import FeatureCard from './FeatureCard';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 function MainContent() {
     const features = [
@@ -20,18 +20,24 @@ function MainContent() {
     ];
 
     return (
-        <main className="container py-5">
-            <h2 className="text-center mb-4">Discover the easiest way to schedule and manage your 1:1 meetings</h2>
-            <div className="row row-cols-1 row-cols-md-3 g-4 text-center">
-                {features.map((feature, index) => (
-                    <FeatureCard key={index} title={feature.title} text={feature.text} />
-                ))}
-            </div>
+        <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+            <main className="container py-5">
+                <h2 className="text-center mb-4">Discover the easiest way to schedule and
+                    manage your 1:1 meetings</h2>
+                <div className="row row-cols-1 row-cols-md-3 g-4 text-center">
+                    {features.map((feature, index) => (
+                        <FeatureCard key={index} title={feature.title}
+                                     text={feature.text}/>
+                    ))}
+                </div>
 
-            <div className="text-center mt-5">
-                <Link to="/signup" className="btn btn-lg btn-light fw-bold border-white btn-log-in">Get Started</Link>
-            </div>
-        </main>
+                <div className="text-center mt-5">
+                    <Link to="/signup"
+                          className="btn btn-lg btn-light fw-bold border-white btn-log-in">Get
+                        Started</Link>
+                </div>
+            </main>
+        </div>
     );
 }
 
