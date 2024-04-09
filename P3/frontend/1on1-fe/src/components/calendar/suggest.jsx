23f00b64dcae9efaa254412837865b73d79a280b
Robert <robert.zhao@mail.utoronto.ca>
2024-04-09 13:56:59 -0400

    added event detail modal

--- P3/frontend/1on1-fe/src/components/calendar/suggest.jsx
+++ P3/frontend/1on1-fe/src/components/calendar/suggest.jsx
@@ -1,21 +1,45 @@
 import React, {useEffect, useState} from 'react';
-import { Table, Modal, ModalHeader, ModalBody, ModalFooter, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { ButtonGroup, Modal, ModalHeader, ModalBody, ModalFooter, Button, InputGroup, InputGroupText, Input, Label, FormGroup, CustomInput }from 'reactstrap';
 import axios from 'axios';
 import {convertToAvailability, convertToTimeSlots, fetchUserId} from "../../utils/utils";
 import Header from '../header/Header';
 import {DraggableSelector} from "react-draggable-selector";
 
 
-
-function Suggest() {
-    const [suggestedCalendar, setSuggestedCalendar] = useState([]);    // State to store suggested calendar
+const Suggest = ({ isOpen, toggle }) => {
     const startDay = new Date('2024-01-01');
     const [dates, setDates] = useState(Array.from({length: 5}, (_, i) => {
         const date = new Date(startDay);
         date.setDate(startDay.getDate() + i);
         return date;
     }));
-    const [timeSlots, setTimeSlots] = useState([]);
+    const [timeSlots, setTimeSlots] = useState(
+        []);
+
+    const [startTime, setStartTime] = useState('10:05 AM');
+    const [endTime, setEndTime] = useState('10:05 AM');
+    const [showAs, setShowAs] = useState('busy');
+    const [participant, setParticipant] = useState('');
+    const [participants, setParticipants] = useState([]);
+  
+    const handleAddParticipant = () => {
+      if (participant.trim() !== '') {
+        setParticipants([...participants, participant]);
+        setParticipant('');
+      }
+    };
+  
+
+
+  const handleRemoveParticipant = (index) => {
+    const updatedParticipants = participants.filter((_, i) => i !== index);
+    setParticipants(updatedParticipants);
+  };
+  const [selectedOption, setSelectedOption] = useState('busy'); // Assuming 'busy' is the default selected option
+
+  const handleOptionChange = (option) => {
+    setSelectedOption(option);
+  };
 
     useEffect(() => {
         const fetchSuggestedCalendar = async () => {
@@ -35,7 +59,7 @@ function Suggest() {
             
         };
     } , []);
-    
+
 
     
     return (
@@ -79,216 +103,45 @@ function Suggest() {
                 </div>
             </div>
         </div>
+        <Modal isOpen={isOpen} toggle={toggle}>
+      <ModalHeader toggle={toggle}>Event detail</ModalHeader>
+      <ModalBody>
+        <h4>Meeting time</h4>
+        <FormGroup>
+          <Label>Starts:</Label>
+          <Input type="time" value={startTime} onChange={(e) => setStartTime(e.target.value)} />
+        </FormGroup>
+        <FormGroup>
+          <Label>Ends:</Label>
+          <Input type="time" value={endTime} onChange={(e) => setEndTime(e.target.value)} />
+        </FormGroup>
+        <h4>Show As</h4>
+        <ButtonGroup aria-label="Avail-non avail selector">
+      <Button color="danger" onClick={() => handleOptionChange('busy')} active={selectedOption === 'busy'}>Busy</Button>
+      <Button color="success" onClick={() => handleOptionChange('free')} active={selectedOption === 'free'}>Free</Button>
+    </ButtonGroup>
+        <h4>Participant</h4>
+        <InputGroup>
+          <InputGroupText>@</InputGroupText>
+          <Input value={participant} onChange={(e) => setParticipant(e.target.value)} placeholder="Username" />
+          <Button color="primary" onClick={handleAddParticipant}>Add</Button>
+        </InputGroup>
+        <ul className="list-group list-group-vertical">
+          {participants.map((participant, index) => (
+            <li key={index} className="list-group-item">
+              {participant}
+              <Button close onClick={() => handleRemoveParticipant(index)} />
+            </li>
+          ))}
+        </ul>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="secondary" onClick={toggle}>Close</Button>
+        <Button color="primary" onClick={toggle}>Save</Button>
+      </ModalFooter>
+    </Modal>
         </div>
             );
-}
+};
 
 export default Suggest;
-{
-
-    /*
-<div id="liveAlertPlaceholder"></div>
-                <button class="btn btn-primary d-inline-flex align-items-center" type="button" id="liveAlertBtn"
-                        data-bs-target="#calendarCarousel">
-                    Pick this one&nbsp;
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                         class="bi bi-arrow-right" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd"
-                              d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
-                    </svg>
-
-                </button>
-            </div>
-     <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<script>
-    const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-    const appendAlert = (message, type) => {
-        const wrapper = document.createElement('div')
-        wrapper.innerHTML = [
-            `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-            `   <div>${message}</div>`,
-            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-            '</div>'
-        ].join('')
-
-        alertPlaceholder.append(wrapper)
-    }
-
-    const alertTrigger = document.getElementById('liveAlertBtn')
-    if (alertTrigger) {
-        alertTrigger.addEventListener('click', () => {
-            appendAlert('Invitation sent to your contacts!', 'success')
-        })
-    }
-</script>
-<script>
-// <!--    for demo, probability trigger ok/not ok-->
-document.getElementById("save").onclick = function () {
-    const randomNumber=Math.random();
-    if (randomNumber<0.5){
-        const okAlert = document.getElementById("okToast");
-            const bsAlert = new bootstrap.Toast(okAlert);
-            bsAlert.show();
-    }else {
-        const okAlert = document.getElementById("notOKToast");
-            const bsAlert = new bootstrap.Toast(okAlert);
-            bsAlert.show();
-    }
-}
-
-<!--    For future use, of/not ok toast-->
-    // document.getElementById("okToastBtn").onclick = function () {
-    //     const okAlert = document.getElementById("okToast");
-    //     const bsAlert = new bootstrap.Toast(okAlert);
-    //     bsAlert.show();
-    // }
-    //
-    // document.getElementById("notOKToastBtn").onclick = function () {
-    //     const okAlert = document.getElementById("notOKToast");
-    //     const bsAlert = new bootstrap.Toast(okAlert);
-    //     bsAlert.show();
-    // }
-</script> */
-}
-
-//  {/* <!--For ok/not ok alert--> */}
-//  <div class="toast-container position-fixed bottom-0 end-0 p-3">
-//  <div id="okToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-//      <div class="toast-header">
-//          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-//               class="bi bi-calendar2-check-fill" viewBox="0 0 16 16">
-//              <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5m9.954 3H2.545c-.3 0-.545.224-.545.5v1c0 .276.244.5.545.5h10.91c.3 0 .545-.224.545-.5v-1c0-.276-.244-.5-.546-.5m-2.6 5.854a.5.5 0 0 0-.708-.708L7.5 10.793 6.354 9.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z"/>
-//          </svg>
-//          <strong class="me-auto">Calendar</strong>
-//          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-//      </div>
-//      <div class="toast-body">
-//          Works for everyone! Meeting time Changed!
-//      </div>
-//  </div>
-// </div>
-// <div class="toast-container position-fixed bottom-0 end-0 p-3">
-//  <div id="notOKToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-//      <div class="toast-header">
-//          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-//               class="bi bi-calendar2-x-fill" viewBox="0 0 16 16">
-//              <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5m9.954 3H2.545c-.3 0-.545.224-.545.5v1c0 .276.244.5.545.5h10.91c.3 0 .545-.224.545-.5v-1c0-.276-.244-.5-.546-.5m-6.6 5.146a.5.5 0 1 0-.708.708L7.293 10l-1.147 1.146a.5.5 0 0 0 .708.708L8 10.707l1.146 1.147a.5.5 0 0 0 .708-.708L8.707 10l1.147-1.146a.5.5 0 0 0-.708-.708L8 9.293z"/>
-//          </svg>
-//          <strong class="me-auto">Calendar</strong>
-//          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-//      </div>
-//      <div class="toast-body">
-//          Someone can't make it! Meeting time is unchanged.
-//      </div>
-//  </div>
-// </div>
-// </div>
-
-// {/* <!--    End of ok/not ok alert--> */}
-
-// {/* <!--   For event detail change --> */}
-// <div class="modal fade" id="eventModal" tabindex="-1" aria-labelledby="eventModalLabel" aria-hidden="true">
-// <div class="modal-dialog">
-//  <div class="modal-content">
-//      <div class="modal-header">
-//          <h1 class="modal-title fs-5" id="eventModalLabel">Event detail</h1>
-//          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-//      </div>
-//      <div class="modal-body">
-//          <h4>Meeting time</h4>
-//          <h5>Starts:</h5>
-//          <div class="cs-form">
-//              <input type="time" class="form-control" value="10:05 AM" placeholder="2:00 PM">
-//          </div>
-//          <h5>Ends:</h5>
-//          <div class="cs-form">
-//              <input type="time" class="form-control" value="10:05 AM" placeholder="3:00 PM">
-//          </div>
-//          <h4>Show As</h4>
-//          <div class="btn-group" role="group" aria-label="Avail-non avail selector">
-//              <input type="radio" class="btn-check" name="btnradio" id="btnNonAvail" autocomplete="off"
-//                     checked>
-//              <label class="btn btn-danger" for="btnNonAvail">Busy</label>
-
-//              <input type="radio" class="btn-check" name="btnradio" id="btnAvail" autocomplete="off">
-//              <label class="btn btn-success" for="btnAvail">Free</label>
-//          </div>
-//          <h4>Participant</h4>
-//          <div class="input-group mb-3">
-//              <span class="input-group-text" id="basic-addon1">@</span>
-//              <input type="text" class="form-control" placeholder="Username" aria-label="Username"
-//                     aria-describedby="basic-addon1">
-//              <button class="btn btn-outline-primary" type="button" id="button-add">Add</button>
-//          </div>
-//          <ul class="list-group list-group-vertical">
-//              <li class="list-group-item">Jennifer Xi
-//                  <button type="button" class="btn-close"></button>
-//              </li>
-//              <li class="list-group-item">Ocean Chen
-//                  <button type="button" class="btn-close"></button>
-//              </li>
-//              <li class="list-group-item">Robert Zhao
-//                  <button type="button" class="btn-close"></button>
-//              </li>
-//              <li class="list-group-item">Kailas Moon
-//                  <button type="button" class="btn-close"></button>
-//              </li>
-//          </ul>
-//      </div>
-//      <div class="modal-footer">
-//          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-//          <button type="button" class="btn btn-primary" data-bs-dismiss="modal" id="save">Save</button>
-//      </div>
-//  </div>
-// </div>
-// </div>
-
-
-// {/* <!--    end of detail change--> */}
-
-// {/* <!--    For select people to meet--> */}
-// <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#peopleModal"
-//  data-bs-whatever="" >
-// Select people to meet
-// </button>
-// <div class="modal fade" id="peopleModal" tabindex="-1" aria-labelledby="eventModalLabel" aria-hidden="true">
-// <div class="modal-dialog">
-//  <div class="modal-content">
-//      <div class="modal-header">
-//          <h1 class="modal-title fs-5" id="peopleModalLabel">Event detail</h1>
-//          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-//      </div>
-//      <div class="modal-body">
-//          <h4>Participant</h4>
-//          <div class="input-group mb-3">
-//              <span class="input-group-text" id="peopleBasic-addon1">@</span>
-//              <input type="text" class="form-control" placeholder="Username" aria-label="Username"
-//                     aria-describedby="basic-addon1">
-//              <button class="btn btn-outline-primary" type="button" id="peopleButton-add">Add</button>
-//          </div>
-//          <ul class="list-group list-group-vertical">
-//              <li class="list-group-item">
-//                  <input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox">
-//                  <label class="form-check-label" for="firstCheckbox">Jennifer Xi</label>
-//              </li>
-//              <li class="list-group-item">
-//                  <input class="form-check-input me-1" type="checkbox" value="" id="secondCheckbox">
-//                  <label class="form-check-label" for="secondCheckbox">Ocean Chen</label>
-//              </li>
-//              <li class="list-group-item">
-//                  <input class="form-check-input me-1" type="checkbox" value="" id="thirdCheckbox">
-//                  <label class="form-check-label" for="thirdCheckbox">Robert Zhao</label>
-//              </li>
-//              <li class="list-group-item">
-//                  <input class="form-check-input me-1" type="checkbox" value="" id="forthCheckbox">
-//                  <label class="form-check-label" for="forthCheckbox">Kailas Moon</label>
-//              </li>
-//          </ul>
-//      </div>
-//      <div class="modal-footer">
-//          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-//          <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Confirm</button>
-//      </div>
-//  </div>
-// </div>
-// </div>
