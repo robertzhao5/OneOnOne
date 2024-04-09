26f1e2e4bc66fec5ef46774b59db1b819c1251cf
oceanchen <oceanchenn@gmail.com>
2024-04-08 22:31:52 -0400

    fixed errors

--- P3/frontend/1on1-fe/src/App.js
+++ P3/frontend/1on1-fe/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 // import bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import React, {useEffect} from 'react';
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import LandingPage from './components/cover/landing/LandingPage';
 import SignupPage from "./components/cover/SignupPage";
 import LoginPage from "./components/cover/LoginPage";
@@ -11,32 +11,34 @@ import AboutPage from "./components/cover/AboutPage";
 import ListContacts from './components/contacts/ListContact';
 
 function App() {
+    let isAuthenticated = false
 
-  useEffect(() => {
-    // Add Bootstrap styling to the body element
-    document.body.classList.add("d-flex", "h-100", "text-center", "text-bg-dark");
-    // Remove the added Bootstrap styling when the component unmounts
-    return () => {
-        document.body.classList.remove("d-flex", "h-100", "text-center", "text-bg-dark");
-    };
-  }, []);
+    useEffect(() => {
+        // Add Bootstrap styling to the body element
+        document.body.classList.add("d-flex", "h-100", "text-center", "text-bg-dark");
+        // Remove the added Bootstrap styling when the component unmounts
+        return () => {
+            document.body.classList.remove("d-flex", "h-100", "text-center", "text-bg-dark");
+        };
+    }, []);
 
-  useEffect(() => {
-      // set dark mode on html
-      document.documentElement.setAttribute('data-bs-theme', 'dark');
-  }, []);
+    useEffect(() => {
+        // set dark mode on html
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
+    }, []);
 
-  return (
-    <BrowserRouter>
-      <Routes>
-          <Route path="/" element={isAuthenticated ? <Navigate to="/Dashbord" /> : <Navigate to="/landing" />} />
-          <Route path="/contacts" element={<ListContacts />} />
-          <Route path="/signup" element={<SignupPage/>}/>
-          <Route path="/login" element={<LoginPage/>}/>
-          <Route path="/about" element={<AboutPage/>}/>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={isAuthenticated ? <Navigate to="/Dashbord"/> :
+                    <Navigate to="/landing"/>}/>
+                <Route path="/contacts" element={<ListContacts/>}/>
+                <Route path="/signup" element={<SignupPage/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/about" element={<AboutPage/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
