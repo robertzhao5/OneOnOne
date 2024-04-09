8df3b98843ed4070ff1f8886c56e7890a08a7324
oceanchen <oceanchenn@gmail.com>
2024-04-08 22:25:54 -0400

    removed redundant urls

--- P3/backend/OneonOne/accounts/urls.py
+++ P3/backend/OneonOne/accounts/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from .views import ChangePasswordView, RegisterView, UserProfileView
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(),
-         name='token_refresh'),
+#     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+#     path('api/token/refresh/', TokenRefreshView.as_view(),
+#          name='token_refresh'),
     path('api/register/', RegisterView.as_view(), name='register'),
     path('api/profile/', UserProfileView.as_view(), name='profile'),
     path('api/change-password/', ChangePasswordView.as_view(),
