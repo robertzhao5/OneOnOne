5761fa125ee31d2c55baf8972fef9fe56f7d5e3d
jenniferxi <jennifer.xi@mail.utoronto.ca>
2024-04-08 22:40:02 -0400

    Switched NavLink syntax

--- P3/frontend/1on1-fe/src/components/header/Header.jsx
+++ P3/frontend/1on1-fe/src/components/header/Header.jsx
@@ -17,9 +17,9 @@ function Header() {
             <div>
                 <h3 className="float-md-start mb-0">1-On-1</h3>
                 <nav className="nav nav-masthead justify-content-center float-md-end">
-                    <NavLink className="nav-link fw-bold py-1 px-0" href="/dashboard">Dashboard</NavLink>
-                    <NavLink className="nav-link fw-bold py-1 px-0" href="/suggest">Suggested</NavLink>
-                    <NavLink className="nav-link fw-bold py-1 px-0" href="/event-details" aria-current="page">Event Details</NavLink>
+                    <NavLink className="nav-link fw-bold py-1 px-0" to="/dashboard">Dashboard</NavLink>
+                    <NavLink className="nav-link fw-bold py-1 px-0" to="/suggest">Suggested</NavLink>
+                    <NavLink className="nav-link fw-bold py-1 px-0" to="/event-details" aria-current="page">Event Details</NavLink>
                     <NavLink className="nav-link fw-bold py-1 px-0" to="/contacts">Contacts</NavLink>
                     <a className="nav-link fw-bold py-1 px-0" href="#" data-bs-toggle="modal" data-bs-target="#signOutModal">Sign out</a>
                 </nav>
