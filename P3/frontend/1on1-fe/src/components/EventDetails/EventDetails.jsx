8c86e59670a20d8cba33dfa9f3554a3f40707519
jenniferxi <jennifer.xi@mail.utoronto.ca>
2024-04-09 15:02:52 -0400

    Re-added EventDetails due to conflicts in routing

---
+++ P3/frontend/1on1-fe/src/components/EventDetails/EventDetails.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../header/Header";
+
+
+function Dashboard() {
+
+    return (
+        <div className="container py-5">
+            <Header/>
+        </div>
+    )
+}
+
+export default Dashboard;
